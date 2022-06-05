838d7c2a50ee98839e89c580f8820ed68e9748a6
Elvin Palma <enpalma@unah.hn>
2022-06-05 08:26:39 -0600

    Agregando Arquitectura basica

---
+++ ComputerWorld/ComputerWorld.BL/ProductosBL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerWorld.BL
+{
+    public class ProductosBL
+    {
+        public List<Producto> ObtenerProductos()
+        {
+            var producto1 = new Producto();
+            producto1.Id = 1;
+            producto1.Descripcion = "Computadora Dell";
+            producto1.Precio = 12500;
+
+            var producto2 = new Producto();
+            producto2.Id = 2;
+            producto2.Descripcion = "Computadora HP";
+            producto2.Precio = 14000;
+
+            var producto3 = new Producto();
+            producto3.Id = 3;
+            producto3.Descripcion = "Computadora Toshiba";
+            producto3.Precio = 13500;
+
+            var listadeProductos = new List<Producto>();
+            listadeProductos.Add(producto1);
+            listadeProductos.Add(producto2);
+            listadeProductos.Add(producto3);
+
+            return listadeProductos;
+        }
+    }
+}
--- ComputerWorld/ComputerWorld.Web/Controllers/ProductosController.cs
+++ ComputerWorld/ComputerWorld.Web/Controllers/ProductosController.cs
@@ -1,4 +1,4 @@
-﻿using ComputerWorld.Web.Models;
+﻿using ComputerWorld.BL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,23 +12,9 @@ namespace ComputerWorld.Web.Controllers
         // GET: Productos
         public ActionResult Index()
         {
-            var producto1 = new ProductoModel();
-            producto1.Id = 1;
-            producto1.Descripcion = "Computadora Dell";
-
-            var producto2 = new ProductoModel();
-            producto2.Id = 2;
-            producto2.Descripcion = "Computadora HP";
-
-            var producto3 = new ProductoModel();
-            producto3.Id = 3;
-            producto3.Descripcion = "Computadora Toshiba";
-
-            var listadeProductos = new List<ProductoModel>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
-
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductos();
+                
             return View(listadeProductos);
         }
     }
